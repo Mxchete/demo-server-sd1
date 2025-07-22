6eb8763abc95c5ec25a562e7487d3fc2859da7a6
Mxchete <elliott.damato@gmail.com>
2025-07-21 20:58:03 -0400

    Moved scripts from symlink to real file

---
+++ scripts/start_container.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME="$1"
+IMAGE_NAME="$2"
+WORKSPACE_DIR="${PWD}"
+
+if ! podman image exists $IMAGE_NAME; then
+  echo "Building the Docker image: $IMAGE_NAME"
+  podman build -t $IMAGE_NAME .
+fi
+
+podman run -it --rm \
+  --name $CONTAINER_NAME \
+  -v "$WORKSPACE_DIR":/workspace \
+  -p 3000:3000 \
+  $IMAGE_NAME
